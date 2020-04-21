39d0fb172110ae91dddced4b3982f83e6aa19477
swag31415 <kidswagatam18@gmail.com>
2020-04-20 22:45:46 -0400

    Added Nodeverse module with 'add' and 'print_all' methods

---
+++ src/nodeverse.nim
@@ -0,0 +1,19 @@
+## The Node Universe
+
+type
+  Node* = object
+    x*, y*: int
+
+type
+  Nodeverse* = object
+    nodes: seq[Node]
+
+method add*(this: var Nodeverse, node: Node) {.base.} =
+  this.nodes.add(node)
+
+method print*(this: Nodeverse) {.base.} =
+  if this.nodes.len() == 0:
+    echo "Nodeverse empty"
+    return
+  for node in this.nodes:
+    echo "(", node.x, ", ", node.y, ")"
