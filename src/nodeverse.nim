724e4ed7eca5e9fefb6a215dbac1809866f114e0
swag31415 <kidswagatam18@gmail.com>
2020-04-22 16:46:33 -0400

    Added a rudimentary evolve method

--- src/nodeverse.nim
+++ src/nodeverse.nim
@@ -1,6 +1,7 @@
 ## The Node Universe
 
 import math
+import random
 
 type
   Node* = object
@@ -22,6 +23,22 @@ method add*(this: var Nodeverse, id: string, x, y: int) {.base.} =
 
 proc dist_squared(a, b: Node): int =
   (a.x-b.x)^2 + (a.y-b.y)^2
+
+method get_dist_to_closest(this: Nodeverse, from_node: Node): int {.base.} =
+  result = dist_squared(this.nodes[0], from_node)
+  for i in 1..this.nodes.high:
+    if result == 0: return
+    let dist = dist_squared(this.nodes[i], from_node)
+    if dist < result: result = dist
+
+method move_random(node: var Node, max_dist: int) {.base.} =
+  node.x.inc(rand(max_dist) - (max_dist div 2))
+  node.y.inc(rand(max_dist) - (max_dist div 2))
+
+method evolve*(this: var Nodeverse) {.base.} =
+  for node in this.nodes.mitems:
+    let dist = this.get_dist_to_closest(node)
+    if dist < 2 or dist > 5: node.move_random(5)
 
 method print*(this: Nodeverse) {.base.} =
   if this.nodes.len() == 0:
